56b6b86391a0a8ddd180c93f6b15244d2daf7486
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2021-01-24 12:14:51 +0700

    feat(server): create simple server and make route GET to /

--- server/app.ts
+++ server/app.ts
@@ -1,1 +1,12 @@
-console.log('hi !')
+import { create } from 'json-server';
+
+const app = create()
+const PORT = 8080
+
+app.get('/', (req, res, next) => {
+  res.json('server was running')
+})
+
+app.listen(PORT, () => {
+  console.log(`server was running on http://localhost:${PORT}`)
+})
