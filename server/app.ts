d03a2b099bbfb1fb9ca9c333293ac336064e3d82
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2021-01-24 13:06:13 +0700

    feat(server): create scheme, db, and response the root.json

--- server/app.ts
+++ server/app.ts
@@ -1,11 +1,17 @@
-import { create } from 'json-server';
+import { create, defaults, router } from 'json-server'
+import path from 'path'
 
 const app = create()
+const routes = router(path.join(__dirname, 'db', 'root.json'))
+const middlewares = defaults()
 const PORT = 8080
 
 app.get('/', (req, res, next) => {
   res.json('server was running')
 })
+
+app.use(middlewares)
+app.use('/api', routes)
 
 app.listen(PORT, () => {
   console.log(`server was running on http://localhost:${PORT}`)
