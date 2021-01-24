67cd6baf512000708e05239389931a0cfe60f7b9
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2021-01-24 13:42:26 +0700

    feat(middleware): create authMiddleware

--- server/app.ts
+++ server/app.ts
@@ -1,3 +1,4 @@
+import { NextFunction, Request, Response } from 'express'
 import { create, defaults, router } from 'json-server'
 import path from 'path'
 
@@ -6,9 +7,22 @@ const routes = router(path.join(__dirname, 'db', 'root.json'))
 const middlewares = defaults()
 const PORT = 8080
 
+const authMiddleware = (req: Request, res: Response, next: NextFunction) => {
+  const token = req.headers.authorization
+
+  // return next() to continue to routes
+  // learn more about middleware https://expressjs.com/en/guide/using-middleware.html
+  return token ? next() : res.status(401).json({
+    "message": "UNAUTHORIZED. you need authorization header, you can add it with postman (https://www.postman.com/downloads/)"
+  })
+}
+
 app.get('/', (req, res, next) => {
   res.json('server was running')
 })
+
+app.use('/api/users', authMiddleware)
+app.use('/api/todos', authMiddleware)
 
 app.use(middlewares)
 app.use('/api', routes)
